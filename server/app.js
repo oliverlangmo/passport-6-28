677b1f11c3b8b052d9d60485e61419a260c57522
oliverlangmo <oliver.langmo@gmail.com>
2016-06-28 11:10:22 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var pg = require('pg');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.listen(3000, function(){
+console.log('server is listening on 3000');
+
+});
